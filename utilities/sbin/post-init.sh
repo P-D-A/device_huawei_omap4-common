7db04b19dee1c3bce151fe0d314808495dc4e78e
ShevT <shev_t@mail.ru>
2016-06-17 18:32:25 +0300

    post-init.sh: add timer_slack

--- utilities/sbin/post-init.sh
+++ utilities/sbin/post-init.sh
@@ -3,4 +3,9 @@
 # SELinux policy for Viper4Android
 /system/xbin/supolicy --live "allow mediaserver mediaserver_tmpfs:file { read write execute };"
 
+# Timer slack
+mount -t cgroup -o timer_slack none /sys/fs/cgroup
+echo 50000 > /sys/fs/cgroup/timer_slack.min_slack_ns
+echo 100000 > /sys/fs/cgroup/timer_slack.min_slack_suspend_ns
+
 exit;
